5868503db5e43974b8b95d98abaaa710d89496e0
pj_zhong <pj_zhong@163.com>
2024-05-24 22:22:39 +0800

    [week2] day4 task 1.1

--- mini-lsm-starter/src/compact/leveled.rs
+++ mini-lsm-starter/src/compact/leveled.rs
@@ -40,9 +40,34 @@ impl LeveledCompactionController {
 
     pub fn generate_compaction_task(
         &self,
-        _snapshot: &LsmStorageState,
+        snapshot: &LsmStorageState,
     ) -> Option<LeveledCompactionTask> {
-        unimplemented!()
+        let (compact_level, target_size) = {
+            let mut taget_size = vec![0; self.options.max_levels];
+            let mut lower_level_size = self.options.base_level_size_mb as u64;
+            let mut compact_level = self.options.max_levels - 1;
+            for (level, ids) in snapshot.levels.iter().rev() {
+                let size = ids
+                    .iter()
+                    .flat_map(|id| snapshot.sstables.get(id))
+                    .map(|table| table.table_size())
+                    .sum::<u64>();
+
+                taget_size[level - 1] = size;
+                if size < lower_level_size {
+                    compact_level = level - 1;
+                    break;
+                } else {
+                    lower_level_size = size / self.options.level_size_multiplier as u64;
+                }
+            }
+
+            (compact_level, taget_size)
+        };
+
+        //println!("{:?}_{:?}", compact_level, target_size);
+
+        None
     }
 
     pub fn apply_compaction_result(
