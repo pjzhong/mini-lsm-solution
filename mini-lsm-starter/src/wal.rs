38679e3eeac125398af2ddbf4e302cf680a55cb0
pj_zhong <pj_zhong@163.com>
2024-06-18 16:49:38 +0800

    [week2] day6 task1

--- mini-lsm-starter/src/wal.rs
+++ mini-lsm-starter/src/wal.rs
@@ -1,12 +1,13 @@
 #![allow(dead_code)] // REMOVE THIS LINE after fully implementing this functionality
 
-use std::fs::File;
-use std::io::BufWriter;
+use std::fs::{File, OpenOptions};
+use std::io::{BufWriter, Read, Write};
+use std::mem::size_of;
 use std::path::Path;
 use std::sync::Arc;
 
 use anyhow::Result;
-use bytes::Bytes;
+use bytes::{Buf, BufMut, Bytes, BytesMut};
 use crossbeam_skiplist::SkipMap;
 use parking_lot::Mutex;
 
@@ -15,19 +16,61 @@ pub struct Wal {
 }
 
 impl Wal {
-    pub fn create(_path: impl AsRef<Path>) -> Result<Self> {
-        unimplemented!()
+    pub fn create(path: impl AsRef<Path>) -> Result<Self> {
+        let file = OpenOptions::new().create(true).append(true).open(path)?;
+        let file = Arc::new(Mutex::new(BufWriter::new(file)));
+
+        Ok(Self { file })
     }
 
-    pub fn recover(_path: impl AsRef<Path>, _skiplist: &SkipMap<Bytes, Bytes>) -> Result<Self> {
-        unimplemented!()
+    pub fn recover(path: impl AsRef<Path>, skiplist: &SkipMap<Bytes, Bytes>) -> Result<Self> {
+        let read_entry = |file: &mut File, buffer: &mut BytesMut| -> Result<Bytes> {
+            let ken_len = size_of::<u16>();
+            file.read_exact(&mut buffer[0..ken_len])?;
+            let len = buffer.get_u16() as usize;
+
+            buffer.reserve(len);
+            file.read_exact(&mut buffer[0..len])?;
+
+            Ok(Bytes::copy_from_slice(&buffer[0..len]))
+        };
+
+        let mut file = OpenOptions::new().read(true).open(&path)?;
+        let file_len = file.metadata()?.len() as usize;
+        if file_len == 0 {
+            Self::create(path)
+        } else {
+            let length_field_len = size_of::<u16>();
+            let mut buffer = BytesMut::with_capacity(size_of::<u16>());
+            let mut count = 0usize;
+            while count < file_len {
+                buffer.clear();
+                let key = read_entry(&mut file, &mut buffer)?;
+
+                buffer.clear();
+                let value = read_entry(&mut file, &mut buffer)?;
+
+                count += length_field_len * 2 + key.len() + value.len();
+                skiplist.insert(key, value);
+            }
+            Self::create(path)
+        }
     }
 
-    pub fn put(&self, _key: &[u8], _value: &[u8]) -> Result<()> {
-        unimplemented!()
+    pub fn put(&self, key: &[u8], value: &[u8]) -> Result<()> {
+        let mut file = self.file.lock();
+        let mut buffer = BytesMut::with_capacity(key.len() + value.len());
+        buffer.put_u16(key.len() as u16);
+        buffer.put_slice(key);
+        buffer.put_u16(value.len() as u16);
+        buffer.put_slice(value);
+        file.write_all(&buffer)?;
+        Ok(())
     }
 
     pub fn sync(&self) -> Result<()> {
-        unimplemented!()
+        let mut file = self.file.lock();
+        file.get_mut().sync_all()?;
+        Ok(())
     }
 }
